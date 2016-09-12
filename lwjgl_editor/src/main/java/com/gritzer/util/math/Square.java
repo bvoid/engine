0d4e5e4379e75315e5764647751fd90dc5a7916b
gabriel L <gabriel.lambers@gmail.com>
2016-09-12 21:10:29 +0200

    Sq is Rect

--- lwjgl_editor/src/main/java/com/gritzer/util/math/Square.java
+++ lwjgl_editor/src/main/java/com/gritzer/util/math/Square.java
@@ -1,14 +1,9 @@
 package com.gritzer.util.math;
 
-public class Square extends Polygon {
+public class Square extends Rectangle {
 
   public Square(float length) {
-    super();
-    setxPoints(new float[] {0f, length, length, 0f});
-    setYPoints(new float[] {0f, 0f, length, length});
-    setColor(Colors.random());
-    w = (int) length;
-    h = (int) length;
+    super(length, length);
   }
 
 }
