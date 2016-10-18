623113f5f9b1b1f5df204ed71ca41cf1679ed053
Gabriel Lambers <gabriel.lambers@gmail.com>
2016-10-18 21:21:40 +0200

    Refactoring
    EntityInterface with BasicEntity-Implementation

--- engine-core/src/main/java/org/bvoid/engine/scene/camera/Camera.java
+++ engine-core/src/main/java/org/bvoid/engine/scene/camera/Camera.java
@@ -1,6 +1,8 @@
 package org.bvoid.engine.scene.camera;
 
+import org.bvoid.engine.scene.BasicEntity;
 
+public class Camera extends BasicEntity {
 
   private static final long serialVersionUID = 0xA000000000000001L;
 
